e2d65a867c920f38a09ebdd13bff65445d5ceec7
Erik Farkas <farkas.erik.92@gmail.com>
2025-04-10 15:20:01 +0200

    Media conversions removed

--- src/Models/Survey.php
+++ src/Models/Survey.php
@@ -14,11 +14,10 @@ use MattDaneshvar\Survey\Contracts\Survey as SurveyContract;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\Translatable\HasTranslations;
-use App\Traits\HasCardImageConversion;
 
 class Survey extends Model implements SurveyContract, HasMedia
 {
-    use HasTranslations, InteractsWithMedia, HasCardImageConversion;
+    use HasTranslations, InteractsWithMedia;
 
     /**
      * Survey constructor.
